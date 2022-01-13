f364ee4c8e8127252b2683ea6c387dbae4cdcd77
Prof-Larry <ujjwalshirpurkar1@gmail.com>
2022-01-13 11:41:15 +0530

    changes

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,10 +1,10 @@
 
 const rotationPlate = document.querySelector('.rotation-plate');
+const darkPlate = document.querySelector('.header');
 
 
 const explore = document.getElementById('explore-button');
 explore.addEventListener('click', function () {
-    // const raje = document.getElementById('name');
-    // raje.style.color = "yellow";
     rotationPlate.classList.replace("rotation-plate", "rotation-after");
+    darkPlate.classList.replace("header", "header-after");
 })
