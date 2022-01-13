f0cd2d2fd9a7f17f20db115bfe3db6070507d73a
Prof-Larry <Ujjwalshirpurkar1@gmail.com>
2022-01-13 16:43:02 +0530

    changes

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,10 +1,11 @@
+const rotationPlate = document.querySelector(".rotation-plate");
+const darkPlate = document.querySelector(".header");
+const myName = document.getElementById("name");
+const header_after_h2 = document.querySelector(".header-after");
 
-const rotationPlate = document.querySelector('.rotation-plate');
-const darkPlate = document.querySelector('.header');
-
-
-const explore = document.getElementById('explore-button');
-explore.addEventListener('click', function () {
-    rotationPlate.classList.replace("rotation-plate", "rotation-after");
-    darkPlate.classList.replace("header", "header-after");
-})
+const explore = document.getElementById("explore-button");
+explore.addEventListener("click", function () {
+  rotationPlate.classList.replace("rotation-plate", "rotation-after");
+  darkPlate.classList.replace("header", "header-after");
+  myName;
+});
